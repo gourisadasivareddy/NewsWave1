e7d589da18eac59de1e7fc7e5cb02d3b82bc5540
siva <sada.siva833@gmail.com>
2023-12-04 15:44:13 +0000

    changes

--- app/src/main/java/com/example/newswave/DestinationScreen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newswave
-
-sealed class DestinationScreen(val route: String) {
-    object SplashScreenDest : DestinationScreen(route = "splash_screen")
-    object MainScreenDest : DestinationScreen(route = "main_screen")
-    object LoginScreenDest : DestinationScreen(route = "login_screen")
-}
